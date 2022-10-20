c647e77bff61e490e4819144a12f8c5119ec2384
bembijgarn <sadwdwa2145@gmail.com>
2022-10-21 02:16:40 +0400

    HackerRank

---
+++ HackerrankSolutions/MigratoryBirds/MigratoryBirds/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<int> arr)
+    {
+
+        int count = 0;
+        int result = 0;
+        int check = 0;
+        var freq = from x in arr group x by x into y select y;
+
+        foreach (var item in freq)
+        {
+            if (item.Count() > count)
+            {
+                count = item.Count();
+                result = item.Key;
+                check = item.Key;
+            }
+            else if (item.Count() == count && item.Key < check)
+            {
+                result = item.Key;
+                check = item.Key;
+
+            }
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.migratoryBirds(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
