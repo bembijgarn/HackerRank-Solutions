4252e44c7048a8c814ba5528ba249e522c9652b5
bembijgarn <sadwdwa2145@gmail.com>
2022-10-21 03:03:13 +0400

    BubbleSort

---
+++ HackerrankSolutions/BubbleSort/ConsoleApp15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+namespace BuuBleSORT
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool swapped = false;
+            int[] arr = { 18, 29, 18, 37, 3, 41, 12, 13, 22, 5, 35, 27, 10 };
+            Console.WriteLine("BEFORE");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+            do
+            {
+                swapped = false;
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                        swapped = true;
+                    }
+                   
+                }
+
+            } while (swapped);
+            Console.WriteLine("AFTER");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
