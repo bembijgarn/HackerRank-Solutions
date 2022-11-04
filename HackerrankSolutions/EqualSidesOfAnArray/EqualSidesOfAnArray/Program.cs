56d4c7c035d86ee5ee9bb8d9702f69192d290e90
bembijgarn <sadwdwa2145@gmail.com>
2022-11-04 17:33:07 +0400

    CodeWars

---
+++ HackerrankSolutions/EqualSidesOfAnArray/EqualSidesOfAnArray/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Kata
+{
+    public static int FindEvenIndex(int[] arr)
+    {
+        int result = -1;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            right = 0;
+            if (i != 0)
+            {
+                left += arr[i - 1];
+            }
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                right += arr[j];
+            }
+            if (right == left)
+            {
+                result = i;
+            }
+
+        }
+        return result;
+    }
+}
